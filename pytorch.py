1ab177441219d1fe922b57f1bcb627578aa166e8
triwahyuu <triwahyu34@gmail.com>
2020-04-08 12:37:32 +0800

    finishing pytorch

--- pytorch.py
+++ pytorch.py
@@ -38,30 +38,30 @@ class PolyRegression(nn.Module):
         return self.weight.detach().numpy().flatten()
 
 
-def generate_data(num_iterate=1000, num_data=100):
-    for _ in range(num_iterate):
-        x = torch.rand(num_data) * 2 - 1            # interval of (0, 1]
-        y = 0.5*x.pow(3) + 7*x.pow(2) + 5*x + 2
-        yield x, y
-
 def compute_r2(y, yreg):
-    y = np.array(y)
-    yreg = np.array(yreg)
+    if isinstance(y, torch.Tensor):
+        y = np.array(y.detach())
+    if isinstance(yreg, torch.Tensor):
+        yreg = np.array(yreg.detach())
 
     ss_tot = np.square(y - y.mean()).sum()
     ss_res = np.square(y - yreg).sum()
     return 1 - (ss_res/ss_tot)
 
-if __name__ == "__main__":
-    torch.manual_seed(123)
-    
-    model = PolyRegression(3)
+def normalise(x):
+    return (x - x.mean()) / x.std()
+
+def fit(model, x, y, iteration=1000):
+    if not isinstance(x, torch.Tensor):
+        x = torch.tensor(x) 
+    if not isinstance(y, torch.Tensor):
+        y = torch.tensor(y)
+
     criterion = nn.MSELoss()
     optim = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.95)
 
     loss_data = []
-
-    for x, y in generate_data(1000, num_data=1000):
+    for _ in range(iteration):
         yreg = model(x)
         loss = criterion(yreg, y)
 
@@ -71,7 +71,29 @@ if __name__ == "__main__":
         loss.backward()
         optim.step()
 
-    print(model.get_weight())
-    print("R2: ", compute_r2(y, yreg.detach()))
+    return model.get_weight(), loss_data
+
+
+if __name__ == "__main__":
+    torch.manual_seed(123)      # uncomment this if you want a non-deterministic result
+
+    x = normalise(torch.randn(10))
+    y = 0.5*x.pow(3) + 7*x.pow(2) + 5*x + 2
+    degree = 3
+
+    model = PolyRegression(degree)
+    weight, loss_data = fit(model, x, y)
+
+    yreg = model(x)
+    print(weight)
+    print("R2: ", compute_r2(y, yreg))
+
+    ## plot regression
+    x_reg = torch.tensor(np.linspace(x.min(), x.max(), 200, endpoint=False), dtype=torch.float32)
+    y_reg = model(x_reg).detach().numpy()
+    x_reg = x_reg.numpy()
+    plt.plot(x_reg, y_reg, color='#a7de77', label=f'Polynomial Regression, degree {degree}')
+    plt.scatter(x, y, color='#9b354c', label='Data Point')
+    plt.legend()
     # plt.plot(loss_data)
-    # plt.show()
+    plt.show()
